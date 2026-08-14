2cc7212ba183abf7cac91cc05019754f7b8aa852
Pratham Saxena <prathamsaxena315@gmail.com>
2026-08-14 13:48:16 +0530

    Time: 1 ms (100%), Space: 43.4 MB (79.53%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int maxLength = 0;
+        int[] count = new int[26];
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            count[rightChar - 'a']++;
+            while (count[rightChar - 'a'] > 2) {
+                char leftChar = s.charAt(left);
+                count[leftChar - 'a']--;
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
